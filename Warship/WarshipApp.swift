cd4d9be84cff9aac0eaceb583b33e0a4b0e52ed5
Vladyslav Dikhtiaruk <vlad.dikhtiaruk@gmail.com>
2024-03-14 22:21:49 +0100

    Initial Commit

---
+++ Warship/WarshipApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarshipApp.swift
+//  Warship
+//
+//  Created by Vladyslav Dikhtiaruk on 14/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct WarshipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
